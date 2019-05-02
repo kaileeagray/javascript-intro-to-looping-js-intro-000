021832dfdc79159b9b5c2d90f77cb3d545ba41ea
Kailee Gray <kaileeagray@gmail.com>
2019-05-02 16:38:45 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,12 +1,12 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-    console.log(i, array);
+    // console.log(i, array);
     if (i === 0) {
       array[i] = "I am 1 strange loop.";
-      console.log("I am 1 strange loop.");
+      // console.log("I am 1 strange loop.");
     } else {
       array[i] = `"I am ${i + 1} strange loops."`;
-      console.log(`"I am ${i + 1} strange loops."`);
+      // console.log(`"I am ${i + 1} strange loops."`);
     }
   }
   return array;
